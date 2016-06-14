065edfeebf11e46267adbb8975fad470f7a7b053
Una <i58540041@hotmail.com>
2016-06-14 17:43:38 +0800

    v1

---
+++ 繼承/繼承/kid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 繼承
+{
+    class kid : pa
+    {
+        //public kid()
+        //{
+        //}
+        public kid(string subJob)
+        {
+            SubJob = subJob;
+        }
+    }
+}
---
+++ 繼承/繼承/pa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 繼承
+{
+    class pa
+    {
+        public string CardCode { get; set; }
+        public string SubJob { get; set; }
+
+        public pa()
+        {
+            CardCode = "t1";
+        }
+
+        public pa(string _cardCode)
+        {
+            CardCode = _cardCode;
+        }
+    }
+}
